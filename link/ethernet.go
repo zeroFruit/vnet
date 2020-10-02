bd78b875e14de8bd652c15dbc334d1b082355719
zerofruit <14wnrkim@gmail.com>
2020-10-02 21:52:26 +0900

    feat: add arp table, separate net address

--- link/ethernet.go
+++ link/ethernet.go
@@ -1,6 +1,6 @@
 package link
 
-import "github.com/zeroFruit/vnet/physical"
+import "github.com/zeroFruit/vnet/phy"
 
 const Octet = 8
 
@@ -9,13 +9,13 @@ type EthernetType [2]byte
 type EthernetPayload interface{}
 
 type EthernetHeader struct {
-	dst physical.HardwareAddr
-	src physical.HardwareAddr
+	dst phy.Addr
+	src phy.Addr
 	fcs uint
 }
 
 // TODO: implement me
-func ShouldHandleFrame(intf physical.Interface, header EthernetHeader) bool {
+func ShouldHandleFrame(intf phy.Interface, header EthernetHeader) bool {
 
 	return false
 }
